cacbff7bc172d9c648f2c27393a9adfa6ce4e299
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-05-10 17:24:47 +0100

    Create ToDo Phase 2 version of App

---
+++ src/ToDoAppPhases/AppPhaseTwo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ToDoItem from './components/ToDoItem'
+
+function App() {
+  return (
+    <div className='todo-list'>
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+    </div>
+  )
+}
+
+export default App
