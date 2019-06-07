01577795a0b6aee9dd5e11f3ac50545cd468e355
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-07 10:59:18 +0100

    Create a reusable component that takes properties to display

---
+++ src/components/ContactCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ContactCard() {
+  return (
+    <div classname="contact-card">
+      <img src="http://placekitten.com/300/200" alt=""/>
+      <h3>Mr. Whiskerson</h3>
+      <p>Phone: (212) 555-1234</p>
+      <p>Email: mr.whiskaz@catnam.meow</p>
+    </div>
+  )
+}
+
+export default ContactCard
