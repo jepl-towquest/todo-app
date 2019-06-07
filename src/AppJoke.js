5a57ba3de13aa23f1ef0fde9983febc284c58698
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-07 12:45:01 +0100

    Get joke data from array, map the data to components into a new array and render that new array

--- src/AppJoke.js
+++ src/AppJoke.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import Joke from './components/Joke'
+import jokesData from './data/jokesData'
 
 function App() {
+
+  // Longhand function
+  // const jokeComponents = jokesData.map(function(joke) {
+  //   return (
+  //     <Joke question={joke.question} punchline={joke.punchline} />
+  //   )
+  // })
+
+  // Longhand arrow function
+  // const jokeComponents = jokesData.map((joke) => {
+  //   return (
+  //     <Joke question={joke.question} punchline={joke.punchline} />
+  //   )
+  // })
+
+  // Shorthand arrow function on one line with implicit return as there is only 1 component
+  const jokeComponents = jokesData.map(joke => <Joke question={joke.question} punchline={joke.punchline} />)
+
   return (
     <div>
-      <Joke 
-        joke={{question:"Why did the chicken cross the road?", punchline:"To get to the other side"}}
-        />
-      <Joke 
-        joke={{punchline:"I tried to kidnap my blacksmith but when I turned my back he made a bolt for the door"}}
-        />
-      <Joke 
-        joke={{question:"Why was the broom late for work?", punchline:"It overswept"}}
-        />
-      <Joke 
-        joke={{punchline:"I relabeled all the jars in my wife's spice rack. I'm not in trouble yet but the thyme is cumin"}}
-        />
-      <Joke 
-        joke={{question:"Why does snow white use Samsung?", punchline:"Because of a bad experience with an Apple"}}
-        />
+      {jokeComponents}
     </div>
   )
 }
