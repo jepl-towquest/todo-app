a5be18a1b2cb30d4db3304b3eacda22f71394a0f
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-07 12:44:02 +0100

    Joke data

---
+++ src/data/jokesData.js
@@ -0,0 +1,27 @@
+const jokesData = [
+  {
+    id: 1,
+    question: "Why did the chicken cross the road?",
+    punchline: "To get to the other side"
+  },
+  {
+    id: 2,
+    punchline: "I tried to kidnap my blacksmith but when I turned my back he made a bolt for the door"
+  },
+  {
+    id: 3,
+    question: "Why was the broom late for work?",
+    punchline: "It overswept"
+  },
+  {
+    id: 4,
+    punchline: "I relabeled all the jars in my wife's spice rack. I'm not in trouble yet but the thyme is cumin"
+  },
+  {
+    id: 5,
+    question: "Why does snow white use Samsung?",
+    punchline: "Because of a bad experience with an Apple"
+  }
+]
+
+export default jokesData
