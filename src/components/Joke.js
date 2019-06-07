b48bcd05902e0da7c7ed1f3e5c1494e3629d7998
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-07 12:20:14 +0100

    change text colour of jokes with only a punchline - not using a ternery operator

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -4,7 +4,7 @@ function Joke(props) {
   return (
     <div>
       <p style={{display: props.joke.question ? "block" : "none"}}>Question: {props.joke.question}</p>
-      <p>Pubchline: {props.joke.punchline}</p>
+      <p style={{color: !props.joke.question && '#999666'}}>Pubchline: {props.joke.punchline}</p>
       <hr />
     </div>
   )
