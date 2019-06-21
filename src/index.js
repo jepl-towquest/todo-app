9e3f992153c330c4240ca9811ffa5953a00f0248
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 15:28:02 +0100

    Run AppEvents

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './AppToDo';
+//import App from './AppToDo';
+import App from './OldApps/AppEvents'
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
