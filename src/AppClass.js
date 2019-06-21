46c2677a3b7fe1978d3bbf9b2f190d9f461aa308
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 11:28:51 +0100

    Comment out function to show class still works

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-function App() {
-  return(
-    <div>
-      <p>Code goes here</p>
-    </div>
-  )
+// function App() {
+//   return(
+//     <div>
+//       <h1>Code goes here</h1>
+//     </div>
+//   )
+// }
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Code goes here</h1>
+      </div>
+    )
+  }
 }
 
 export default App
