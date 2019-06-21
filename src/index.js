d8bc08061adc506c8003fa3c00c45715b4f57cc7
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 11:38:45 +0100

    Add AppClassPractice

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './AppToDo';
-import App from './AppClass';
+import App from './AppClassPractice';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
