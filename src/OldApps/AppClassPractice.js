e0676339374fde3a07d1bfeeec8983c28096b054
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 11:51:43 +0100

    Update paths

--- src/OldApps/AppClassPractice.js
+++ src/OldApps/AppClassPractice.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './components/HeaderClass'
-import Greeting from './components/GreetingClass'
+import Header from '../components/HeaderClass'
+import Greeting from '../components/GreetingClass'
 
 class App extends React.Component {
   render() {
