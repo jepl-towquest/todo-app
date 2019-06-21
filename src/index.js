55d3bf15c357400f5b21e53205cbb785782699eb
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 11:40:53 +0100

    Add AppState

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './AppToDo';
-import App from './AppClassPractice';
+//import App from './AppClassPractice';
+import App from './AppState'
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
