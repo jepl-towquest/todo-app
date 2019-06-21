97e5f5c17eb10ad78a9408dc2c4fd29fd3ae4e5c
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 08:23:22 +0100

    Update for ToDo App phase 3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React from 'react'
-//import ToDoItem from './components/ToDoItem'
-import ContactCard from './components/ContactCard'
+import ToDoItem from './components/ToDoItem'
+import todosData from './data/todosData'
 
 function App() {
+  const ToDoItemComponents = todosData.map(todo => <ToDoItem key={todo.id} item={todo} />)
+
   return (
-    // <div className='todo-list'>
-    //   <ToDoItem />
-    //   <ToDoItem />
-    //   <ToDoItem />
-    //   <ToDoItem />
-    // </div>
-    <div classname="contacts">
-      <ContactCard 
-        contact={{name:"Mr. Whiskerson", imgUrl:"http://placekitten.com/300/200", phone:"(212) 555-1234", email:"mr.whiskaz@catnam.meow"}}
-      />
-      <ContactCard 
-        contact={{name:"Flyffykins", imgUrl:"http://placekitten.com/400/200", phone:"(212) 555-2345", email:"fluff@catnam.meow"}}
-      />
-      <ContactCard 
-        contact={{name:"Destroyer", imgUrl:"http://placekitten.com/400/300", phone:"(212) 555-3456", email:"ofworlds@catnam.meow"}}
-      />
-      <ContactCard 
-        contact={{name:"Felix", imgUrl:"http://placekitten.com/200/100", phone:"(212) 555-4567", email:"thecat@catnam.meow"}}
-      />
+    <div className='todo-list'>
+      {ToDoItemComponents}
     </div>
   )
 }
