e10d3af866a23b332437d4baf962c2425e984814
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 16:17:59 +0100

    Utilise the handleChange method in the parent component

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-function handleOnChange() {
-  console.log("Changed!")
-}
-
 function ToDoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.item.completed} onChange={handleOnChange} />
+      <input 
+        type="checkbox" 
+        checked={props.item.completed} 
+        onChange={() => props.handleChange(props.item.id)} />
       <p>{props.item.text}</p>
     </div>    
   )
