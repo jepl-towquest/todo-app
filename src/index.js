0d49f915bd381368b484a5965a0424dcace33fdf
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 11:19:14 +0100

    Add AppClass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './AppToDo';
+import App from './AppClass';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
