1b00e0ef01bbcf880eef2dd7ba0bc3602a9bcf48
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 08:16:53 +0100

    ToDo item data

---
+++ src/data/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Take out the trash",
+    completed: true
+  },
+  {
+    id: 2,
+    text: "Grocery shopping",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Clean out gecko tank",
+    completed: false
+  },
+  {
+    id: 4,
+    text: "Mow lawn",
+    completed: true
+  },
+  {
+    id: 5,
+    text: "Catch up on tv",
+    completed: false
+  }
+]
+
+export default todosData
