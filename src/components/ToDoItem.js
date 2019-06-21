e979732c8efade063e4ade618028d64647c6f278
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 15:31:00 +0100

    Handle the onChange event

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+function handleOnChange() {
+  console.log("Changed!")
+}
+
 function ToDoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.item.completed} />
+      <input type="checkbox" checked={props.item.completed} onChange={handleOnChange} />
       <p>{props.item.text}</p>
     </div>    
   )
