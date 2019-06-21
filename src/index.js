f6512899b2450efc7ff24a7b332c9f0191904426
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 15:57:48 +0100

    Comment AppToDo back in

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './AppToDo';
-import App from './OldApps/AppEventsChangeState'
+import App from './AppToDo';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
