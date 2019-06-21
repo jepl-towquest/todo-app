eab17643687314275adbf249f4bff136d1fd98ec
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 17:20:10 +0100

    Initial commit

---
+++ src/OldApps/AppConditionalRender.js
@@ -0,0 +1,18 @@
+import React, {component} from 'react'
+
+class App extends component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div>
+        Code goes here
+      </div>
+    )
+  }
+}
+
+export default App
