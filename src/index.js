26656ffebfcc3eaf6fc9a750266aea0c1eceac70
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 11:51:58 +0100

    remove old imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './AppToDo';
-//import App from './AppClassPractice';
 import App from './AppState'
 import './index.css';
 
