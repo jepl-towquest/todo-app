7ac451a5a2dcd9586a9ed3ccecb5e0ee50a019a4
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 11:49:26 +0100

    Reference internal state of the component

--- src/AppState.js
+++ src/AppState.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
 class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      answer: "Yes"
+    }
+  }
+
   render() {
     return(
       <div>
-        <h1>Is state important to know?</h1>
+        <h1>Is state important to know? {this.state.answer}</h1>
       </div>
     )
   }
