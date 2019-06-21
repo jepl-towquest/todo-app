12cb2f41e773dc5be36f91e03a4dfc673d9d662b
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 15:54:58 +0100

    Update state using an anonymous function

--- src/OldApps/AppEventsChangeState.js
+++ src/OldApps/AppEventsChangeState.js
@@ -11,7 +11,11 @@ class App extends Component {
   }
 
   handleClick() {
-    this.setState({count: 1})
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
   }
 
   render() {
