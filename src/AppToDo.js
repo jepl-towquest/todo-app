3f4f11b8b7b8464d3f96eda15fcbfacb2107e8d2
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-06-21 14:01:33 +0100

    Load todos data into state

--- src/AppToDo.js
+++ src/AppToDo.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ToDoItem from './components/ToDoItem'
 import todosData from './data/todosData'
 
-function App() {
-  const ToDoItemComponents = todosData.map(todo => <ToDoItem key={todo.id} item={todo} />)
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+  }  
 
-  return (
-    <div className='todo-list'>
-      {ToDoItemComponents}
-    </div>
-  )
+  render() {
+    const ToDoItemComponents = this.state.todos.map(todo => <ToDoItem key={todo.id} item={todo} />)
+
+    return (
+      <div className='todo-list'>
+        {ToDoItemComponents}
+      </div>
+    )
+  }  
 }
 
 export default App
