6f4c8fabd64a32e1e772df06a210e2e788552582
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-07-05 15:59:22 +0100

    Add new Forms APP

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './OldApps/AppFetchApiData';
+import App from './OldApps/AppForms';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
