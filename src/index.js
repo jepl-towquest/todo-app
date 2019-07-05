5ede834681003d6e63ad96ec83388a55633eef05
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-07-05 14:37:20 +0100

    Reference conditional render part 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './AppToDo';
-import App from './OldApps/AppConditionalRender'
+import App from './OldApps/AppConditionalPart2'
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
