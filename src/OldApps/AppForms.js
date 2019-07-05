b6b3eeed636e5e7ac6b8d8ac29dfc389eb96b9ce
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-07-05 15:59:34 +0100

    Initial commit

---
+++ src/OldApps/AppForms.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div>
+        Code goes here
+      </div>
+    )
+  }
+}
+
+export default App
