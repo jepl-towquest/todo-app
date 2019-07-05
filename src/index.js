bf6070b336be37b37600dd40dcf93c7bb3b6268d
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-07-05 09:35:38 +0100

    Add conditional to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './AppToDo';
+//import App from './AppToDo';
+import App from './OldApps/AppConditionalRender'
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
