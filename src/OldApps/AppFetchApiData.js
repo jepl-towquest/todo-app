87911c7d9beaf8bab595ec708a2a49ab0af96a72
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-07-05 15:51:39 +0100

    Conditionally render a loading message until the API returns

--- src/OldApps/AppFetchApiData.js
+++ src/OldApps/AppFetchApiData.js
@@ -6,21 +6,28 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
+      loading: false,
       character: {}
     }
   }
 
   componentDidMount() {
+    this.setState({loading: true})
+
     // Using javascript promises to chain commands
     fetch("https://swapi.co/api/people/1")
       .then(response => response.json()) // turn the response into JSON we can use
-      .then(data => this.setState({character: data}))    
+      .then(data => this.setState({
+        loading: false,
+        character: data}))
   }
 
   render() {
+    const text = this.state.loading ? "Loading..." : this.state.character.name
+
     return(
       <div>
-        Name: {this.state.character.name}
+        <p>{text}</p>
       </div>
     )
   }
