a2a35e89f4b7caa4286f89bacd48720749ecb888
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-07-05 17:04:12 +0100

    Handle the state change in an input text

--- src/OldApps/AppForms.js
+++ src/OldApps/AppForms.js
@@ -1,16 +1,27 @@
 import React, {Component} from 'react'
 
+// https://reactjs.org/docs/forms.html
+
 class App extends Component {
   constructor() {
     super()
     this.state = {}
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      firstName: event.target.value
+    })
   }
 
   render() {
     return(
-      <div>
-        Code goes here
-      </div>
+      <form>
+        <input type="text" placeholder="First Name" onChange={this.handleChange} />
+        <p>{this.state.firstName}</p>
+      </form>
     )
   }
 }
