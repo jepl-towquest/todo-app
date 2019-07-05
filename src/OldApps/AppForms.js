88e5e8a5ea7865454f0c79d14713ea84e64539be
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-07-05 17:09:28 +0100

    Add a name property that matches the property in state so can use handleChange for all text based inputs

--- src/OldApps/AppForms.js
+++ src/OldApps/AppForms.js
@@ -5,22 +5,27 @@ import React, {Component} from 'react'
 class App extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      firstName: "",
+      lastName: ""
+    }
 
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(event) {
     this.setState({
-      firstName: event.target.value
+      [event.target.name]: event.target.value
     })
   }
 
   render() {
     return(
       <form>
-        <input type="text" placeholder="First Name" onChange={this.handleChange} />
-        <p>{this.state.firstName}</p>
+        <input type="text" name="firstName" placeholder="First Name" onChange={this.handleChange} />
+        <br />
+        <input type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange} />
+        <p>{this.state.firstName} {this.state.lastName}</p>
       </form>
     )
   }
