3d3bc5b44cc4516f02c9182d9d800ad13d6e29b2
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-07-05 14:46:29 +0100

    Conditional render practice

---
+++ src/OldApps/AppConditionalPractice.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div>
+        Code goes here
+      </div>
+    )
+  }  
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './AppToDo';
-import App from './OldApps/AppConditionalPart2'
+import App from './OldApps/AppConditionalPractice'
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
